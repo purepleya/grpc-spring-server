932d0b753ff328256a0c4bdf7233c732df954220
jh <purepleya@gmail.com>
2023-12-17 21:28:45 +0900

    Greeter 서비스의 sayHello 함수 정의

---
+++ src/main/java/jh/study/grpcspring/server/helloworld/Greeter.java
@@ -0,0 +1,14 @@
+package jh.study.grpcspring.server.helloworld;
+
+import io.grpc.stub.StreamObserver;
+
+public class Greeter extends GreeterGrpc.GreeterImplBase {
+
+    @Override
+    public void sayHello(HelloRequest request, StreamObserver<HelloReply> responseObserver) {
+        HelloReply reply = HelloReply.newBuilder().setMessage("Hello " + request.getName()).build();
+        responseObserver.onNext(reply);
+        responseObserver.onCompleted();
+    }
+
+}
