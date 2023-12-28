5c6fb507ea81d33f017decefb060619837a74651
jh <purepleya@gmail.com>
2023-12-28 20:41:01 +0900

    server 에 대한 단위 테스트 작성

---
+++ src/test/java/jh/study/grpcspring/server/helloworld/GreeterTest.java
@@ -0,0 +1,43 @@
+package jh.study.grpcspring.server.helloworld;
+
+import io.grpc.inprocess.InProcessChannelBuilder;
+import io.grpc.inprocess.InProcessServerBuilder;
+import io.grpc.testing.GrpcCleanupRule;
+import org.junit.Rule;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GreeterTest {
+
+    /**
+     * This rule manages automatic graceful shutdown for the registered servers and channels at the
+     * end of test.
+     */
+    @Rule
+    public final GrpcCleanupRule grpcCleanup = new GrpcCleanupRule();
+
+
+    @Test
+    public void greeterImpl_replyMessage() throws Exception {
+        // Generate a unique in-process server name.
+        String serverName = InProcessServerBuilder.generateName();
+
+        // Create a server, add service, start, and register for automatic graceful shutdown.
+        grpcCleanup.register(InProcessServerBuilder
+                .forName(serverName)
+                .directExecutor()
+                .addService(new Greeter()).build().start());
+
+        GreeterGrpc.GreeterBlockingStub blockingStub = GreeterGrpc.newBlockingStub(
+                // Create a client channel and register for automatic graceful shutdown.
+                grpcCleanup.register(InProcessChannelBuilder.forName(serverName).directExecutor().build()));
+
+
+        HelloReply reply =
+                blockingStub.sayHello(HelloRequest.newBuilder().setName( "test name").build());
+
+        assertEquals("Hello test name22", reply.getMessage());
+    }
+}
